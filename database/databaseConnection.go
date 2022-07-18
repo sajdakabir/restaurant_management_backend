f5ee2154b248da812b7bb53e030036b7550e7e63
sajdakabir <sajda.kbir.com>
2022-07-18 22:22:18 +0530

    modify the database.go file

--- database/databaseConnection.go
+++ database/databaseConnection.go
@@ -13,12 +13,16 @@ import (
 func DBinstance() *mongo.Client {
 	MongoDb := "mongodb://localhost:27017"
 	fmt.Print(MongoDb)
+
 	client, err := mongo.NewClient(options.Client().ApplyURI(MongoDb))
+
 	if err != nil {
 		log.Fatal(err)
 	}
-	ctx, cancle := context.WithTimeout(context.Background(), 10*time.Second)
-	defer cancle()
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+
+	defer cancel()
+
 	err = client.Connect(ctx)
 	if err != nil {
 		log.Fatal(err)
@@ -30,6 +34,6 @@ func DBinstance() *mongo.Client {
 var Client *mongo.Client = DBinstance()
 
 func OpenCollection(client *mongo.Client, collectionName string) *mongo.Collection {
-	var collection *mongo.Collection = (*mongo.Collection)(client).Database("restaurant").Collection(collectionName)
+	var collection *mongo.Collection = client.Database("restaurant").Collection(collectionName)
 	return collection
 }

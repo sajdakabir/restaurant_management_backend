c14d151febbba034b19ca332a8980852a639615b
sajdakabir <sajda.kbir.com>
2022-07-18 23:26:08 +0530

    added invoice method

--- models/invoiceModel.go
+++ models/invoiceModel.go
@@ -1,18 +1,18 @@
-// package models
+package models
 
-// import (
-// 	"time"
+import (
+	"time"
 
-// 	"go.mongodb.org/mongo-driver/bson/primitive"
-// )
+	"go.mongodb.org/mongo-driver/bson/primitive"
+)
 
-// type Invoice struct {
-// 	ID               primitive.ObjectID `bson:"_id"`
-// 	Invoice_id       string             `json:"invoice_id"`
-// 	Order_id         string             `json:"order_id" validate:"eq=CARD|eq=CASH|eq="`
-// 	Payment_method   *string
-// 	Payment_status   *string
-// 	Payment_due_date time.Time
-// 	Created_at       time.Time
-// 	Updated_at       time.Time
-// }
+type Invoice struct {
+	ID               primitive.ObjectID `bson:"_id"`
+	Invoice_id       string             `json:"invoice_id"`
+	Order_id         string             `json:"order_id"`
+	Payment_method   *string            `json:"payment_method" validate:"eq=CARD|eq=CASH|eq="`
+	Payment_status   *string            `json:"payment_status" validate:"required,eq=PENDING|eq=PAID"`
+	Payment_due_date time.Time          `json:"payment_due_date"`
+	Created_at       time.Time          `json:"created_at"`
+	Updated_at       time.Time          `json:"updated_at"`
+}

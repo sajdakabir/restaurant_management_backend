db869d75b402c128ea4ae19713807297cf697a4b
sajdakabir <sajda.kbir.com>
2022-07-19 09:42:28 +0530

    complete the GetFood function in controller foodController file

--- controllers/foodController.go
+++ controllers/foodController.go
@@ -1,25 +1,29 @@
 package controller
 
-import "github.com/gin-gonic/gin"
+import (
+	"context"
+	"github/sajdakabir/restaurant_management_backend/database"
+	"github/sajdakabir/restaurant_management_backend/models"
+	"net/http"
+	"time"
 
-func GetFoods() gin.HandlerFunc {
-	return func(ctx *gin.Context) {
+	"github.com/gin-gonic/gin"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo"
+)
 
-	}
-}
+var foodCollection *mongo.Collection = database.OpenCollection(database.Client, "food")
 
 func GetFood() gin.HandlerFunc {
-	return func(ctx *gin.Context) {
-
-	}
-}
-func CreateFood() gin.HandlerFunc {
-	return func(ctx *gin.Context) {
-
-	}
-}
-func UpdateFood() gin.HandlerFunc {
-	return func(ctx *gin.Context) {
-
+	return func(c *gin.Context) {
+		var ctx, cancle = context.WithTimeout(context.Background(), 100*time.Second)
+		foodId := c.Param("food_id")
+		var food models.Food
+		err := foodCollection.FindOne(ctx, bson.M{"food_id": foodId}).Decode(&food)
+		defer cancle()
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": "error occured while featching the food item"})
+		}
+		c.JSON(http.StatusOK, food)
 	}
 }

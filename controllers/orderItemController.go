4dd87b33309a67e5756f34b1d4805a7c53993f91
sajdakabir <sajda.kbir.com>
2022-07-28 17:11:30 +0530

    added update OrderItem function

--- controllers/orderItemController.go
+++ controllers/orderItemController.go
@@ -12,6 +12,7 @@ import (
 	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/bson/primitive"
 	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
 type OderItemPack struct {
@@ -69,5 +70,45 @@ func GetOrderItem() gin.HandlerFunc {
 			return
 		}
 		c.JSON(http.StatusOK, orderItem)
+	}
+}
+
+func UpdateOrderItem() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		var ctx, cancel = context.WithTimeout(context.Background(), 100*time.Second)
+		var orderItem models.OrderItem
+		orderItemId := c.Param("order_item_id")
+		filter := bson.M{"order_item_id": orderItemId}
+		var updateObj primitive.D
+		if orderItem.Unit_price != nil {
+			updateObj = append(updateObj, bson.E{"unit_price", *&orderItem.Unit_price})
+		}
+		if orderItem.Quantity != nil {
+			updateObj = append(updateObj, bson.E{"quantity", *orderItem.Quantity})
+		}
+		if orderItem.Food_id != nil {
+			updateObj = append(updateObj, bson.E{"food_id", *orderItem.Food_id})
+		}
+		orderItem.Updated_at, _ = time.Parse(time.RFC3339, time.Now().Format(time.RFC3339))
+		updateObj = append(updateObj, bson.E{"updated_at", orderItem.Updated_at})
+		upsert := true
+		opt := options.UpdateOptions{
+			Upsert: &upsert,
+		}
+		result, err := orderItemCollection.UpdateOne(
+			ctx,
+			filter,
+			bson.D{
+				{"&set", updateObj},
+			},
+			&opt,
+		)
+		if err != nil {
+			msg := "order item update failed"
+			c.JSON(http.StatusInternalServerError, gin.H{"error": msg})
+			return
+		}
+		defer cancel()
+		c.JSON(http.StatusOK, result)
 	}
 }
